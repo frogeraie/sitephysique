e14144ccf2d43c5c4e537102b6218be023429bc4
frogeraie <frogeraie1@wanadoo.fr>
2015-08-02 23:54:09 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,118 @@
+var STREAM_SECRET = process.argv[3],
+	STREAM_PORT = process.argv[4] || 8082,
+	WEBSOCKET_PORT = process.argv[5] || 8084,
+	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+
+var width = 320,
+	height = 240;
+
+// Websocket Server
+var socketServer = new (require('ws').Server)({port: WEBSOCKET_PORT});
+socketServer.on('connection', function(socket) {
+	// Send magic bytes and video size to the newly connected socket
+	// struct { char magic[4]; unsigned short width, height;}
+	var streamHeader = new Buffer(8);
+	streamHeader.write(STREAM_MAGIC_BYTES);
+	streamHeader.writeUInt16BE(width, 4);
+	streamHeader.writeUInt16BE(height, 6);
+	socket.send(streamHeader, {binary:true});
+
+	console.log( 'New WebSocket Connection ('+socketServer.clients.length+' total)' );
+	
+	socket.on('close', function(code, message){
+		console.log( 'Disconnected WebSocket ('+socketServer.clients.length+' total)' );
+	});
+});
+
+socketServer.broadcast = function(data, opts) {
+	for( var i in this.clients ) {
+		if (this.clients[i].readyState == 1) {
+			this.clients[i].send(data, opts);
+		}
+		else {
+			console.log( 'Error: Client ('+i+') not connected.' );
+		}
+	}
+};
+
+
+// HTTP Server to accept incomming MPEG Stream
+var streamServer = require('http').createServer( function(request, response) {
+	var params = request.url.substr(1).split('/');
+
+	if( params[0] == STREAM_SECRET ) {
+		width = (params[1] || 320)|0;
+		height = (params[2] || 240)|0;
+		
+		console.log(
+			'Stream Connected: ' + request.socket.remoteAddress + 
+			':' + request.socket.remotePort + ' size: ' + width + 'x' + height
+		);
+		request.on('data', function(data){
+			socketServer.broadcast(data, {binary:true});
+		});
+	}
+	else {
+		console.log(
+			'Failed Stream Connection: '+ request.socket.remoteAddress + 
+			request.socket.remotePort + ' - wrong secret.'
+		);
+		response.end();
+	}
+}).listen(STREAM_PORT);
+
+console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/<secret>/<width>/<height>');
+console.log('Awaiting WebSocket connections on ws://127.0.0.1:'+WEBSOCKET_PORT+'/');
+
+var serialport = require("serialport"),		// include the serialport library
+	SerialPort  = serialport.SerialPort,	   // make a local instance of serial
+	servi = require('servi'),		// include the servi library
+	app = new servi(false);		// servi instance
+
+// configure the server's behavior:
+app.port(9090);						// port number to run the server on
+app.serveFiles("public");			// serve all static HTML files from /public
+
+
+// respond to web GET requests for the index.html page:
+app.route('/', sendIndexPage);
+app.route('/index*', sendIndexPage);
+// take anything that begins with /output as an LED request:
+app.route('/output/:color/:brightness', sendToSerial);
+
+// now that everything is configured, start the server:
+app.start();	
+console.log("Listening for new clients on port 9090");
+
+ 
+// the third word of the command line command is serial port name:
+var portName = process.argv[2];				  
+// print out the port you're listening on:
+console.log("opening serial port: " + portName);	
+
+// open the serial port. Uses the command line parameter:
+var myPort = new SerialPort(portName, { 
+	baudRate: 9600,
+	// look for return and newline at the end of each data packet:
+	parser: serialport.parsers.readline("\r\n") 
+});
+
+/* The rest of the functions are event-driven. 
+   They only get called when the server gets incoming GET requests:
+*/
+
+// this function responds to a GET request with the index page:
+function sendIndexPage(request) {
+  request.serveFile('/index.html');
+}
+
+function sendToSerial(request) {
+  // get the parameters from the URL:
+  var brightnessCommand = request.params.color + request.params.brightness;
+  console.log("received "+ brightnessCommand);
+
+  // send it out the serial port:
+  myPort.write(brightnessCommand);
+  // send the data and close the connection:
+  request.respond(brightnessCommand);
+}
